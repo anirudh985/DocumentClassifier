89fac3c7e1b1220489ec5e449e63faacb19241e9
= <=>
2016-09-17 18:22:35 -0400

    Minor Optimizations and stored Word To Number Of Docs in a File

--- Preprocessor.py
+++ Preprocessor.py
@@ -2,12 +2,14 @@
 
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
-#from itertools import izip
+from itertools import izip
 from nltk.stem.snowball import SnowballStemmer
 import re
 import urllib2
 import sys
 import math
+import time
+from __future__ import division
 
 class Preprocessor:
 	colIndex = 0
@@ -22,9 +24,15 @@ class Preprocessor:
 	# stopWords = {'the': 1, 'for': 1, 'can': 1, 'could': 1, 'would': 1,'they': 1, 'there': 1, 'and': 1}
 
 	def __init__(self):
-		self.fileFeatureVector = open("./FeatureVector", "w+")
-		self.file_word_corpus_position = open('./wordPosition','w+')
-		self.fileArticleSeekPosition = open("./Article-SeekPosition", "w+")
+		if(sys.argv[1] == 'Y' or sys.argv[1] == 'y'):
+			self.fileFeatureVector = open("./FeatureVector", "r")
+			self.file_word_corpus_position = open('./wordPosition','r')
+			self.file_word_numDocs = open('./wordNumDocs', 'r')
+		else:
+			self.fileFeatureVector = open("./FeatureVector", "w+")
+			self.file_word_corpus_position = open('./wordPosition', 'w+')
+			self.file_word_numDocs = open('./wordNumDocs','w+')
+		# self.fileArticleSeekPosition = open("./Article-SeekPosition", "w+")
 
 	def init_file_parsing(self):
 		self.stopWords = self.createStopWords()
@@ -54,16 +62,19 @@ class Preprocessor:
 					# print self.myTokenizer(body, self.stopWords)
 				articleId = article['NEWID']
 				Preprocessor.TotalDocs += 1
-				self.storeToFiles(self.myTokenizer(title, body), articleId)
+				# self.storeToFiles(self.myTokenizer(title, body), articleId)
+				self.storeFeatureVector(self.myTokenizer(title, body))
 		self.dump_wordcorpus_columnindex_map()
+		self.dump_word_to_numDocs_map()
 		self.file_word_corpus_position.close()
 		self.fileFeatureVector.close()
-		self.fileArticleSeekPosition.close()
+		# self.fileArticleSeekPosition.close()
 
 	def createStopWords(self):
-		stopWordsList = set(stopwords.words('english'))
-		return stopWordsList
-			#dict((word, i) for (word, i) in izip(stopWordsList, range(1,len(stopWordsList)+1)))
+		# stopWordsList = set(stopwords.words('english'))
+		stopWordsList = stopwords.words('english')
+		# return stopWordsList
+		return dict((word, i) for (word, i) in izip(stopWordsList, range(1,len(stopWordsList)+1)))
 
 	def update_word_to_doc_map(self,articleWiseWordFreqMap):
 		for key, value in articleWiseWordFreqMap.iteritems():
@@ -113,12 +124,12 @@ class Preprocessor:
 	def removeTags(self,body):
 		return re.sub('<[^<>]+>', '', body)
 
-	def storeToFiles(self,articleWiseWordFreqMap, articleId):
-		self.storeArticleSeekPosition(articleId)
-		Preprocessor.nextPositionInFile = self.storeFeatureVector(articleWiseWordFreqMap)
-
-	def storeArticleSeekPosition(self,articleId):
-		self.fileArticleSeekPosition.write(str(articleId)+"-"+str(Preprocessor.nextPositionInFile)+"\n")
+	# def storeToFiles(self,articleWiseWordFreqMap, articleId):
+	# 	self.storeArticleSeekPosition(articleId)
+	# 	Preprocessor.nextPositionInFile = self.storeFeatureVector(articleWiseWordFreqMap)
+    #
+	# def storeArticleSeekPosition(self,articleId):
+	# 	self.fileArticleSeekPosition.write(str(articleId)+"-"+str(Preprocessor.nextPositionInFile)+"\n")
 
 	def storeFeatureVector(self,articleWiseWordFreqMap):
 		outputString = ""
@@ -127,46 +138,72 @@ class Preprocessor:
 		outputString = outputString[:-1]
 		outputString += '\n'
 		self.fileFeatureVector.write(outputString)
-		return self.fileFeatureVector.tell()
+		# return self.fileFeatureVector.tell()
+
+	def dumpFile(self, someMap, filehandle, outerSplitSeparator, innerSplitSeparator = '-', headerLine = None):
+		if(headerLine != None):
+			filehandle.write(str(headerLine)+"\n")
+		outputString = ""
+		for key, value in someMap.iteritems():
+			outputString += key + innerSplitSeparator + str(value) + outerSplitSeparator
+		outputString = outputString[:-1]
+		filehandle.write(outputString)
 
 	def dump_wordcorpus_columnindex_map(self):
-		for key, value in Preprocessor.wordCorpusToColIndexMap.iteritems():
-			self.file_word_corpus_position.write(key+"-"+str(value)+"\t")
-		self.file_word_corpus_position.write('\n')
+		self.dumpFile(Preprocessor.wordCorpusToColIndexMap, self.file_word_corpus_position, outerSplitSeparator="\t", headerLine=Preprocessor.TotalDocs)
 
-	def load_wordcorpus_columnindex_map(self):
-		with open("./wordPosition") as fileobject:
+	def dump_word_to_numDocs_map(self):
+		self.dumpFile(Preprocessor.wordToNumDocsMap, self.file_word_numDocs, outerSplitSeparator="\t", headerLine=Preprocessor.TotalDocs)
+
+	def loadLineByLineFileIntoMap(self, filePath, someMap, splitSeparator):
+		with open(filePath) as fileobject:
+			headerValue = int(fileobject.readline())
 			for line in fileobject:
-				(key, value) = line.split('-')
-				Preprocessor.wordCorpusToColIndexMap[key] = int(value)
-				if key not in Preprocessor.wordToNumDocsMap.keys:
-					Preprocessor.wordToNumDocsMap[key] = 1
-				else:
-					Preprocessor.wordToNumDocsMap[key] += 1
-			Preprocessor.TotalDocs += 1
+				(key, value) = line.split(splitSeparator)
+				someMap[key] = int(value)
+		return headerValue
 
-	def construct_feature_vetor_matrix(self, doLoadWordCorpus = 'N', isFeatureTfIdf = 0):
-		if doLoadWordCorpus != 'Y' and doLoadWordCorpus != 'y':
-			self.load_wordcorpus_columnindex_map()
-		self.fileFinalFeatureVector = open("./FinalFeatureVector", "w+")
+	def loadSingleLineFileIntoMap(self, filePath, someMap, outerSplitSeparator, innerSplitSeparator):
+		with open(filePath) as fileobject:
+			headerValue = int(fileobject.readline())
+			entireData = fileobject.readline()
+		for pair in entireData.split(outerSplitSeparator):
+			(key, value) = pair.split(innerSplitSeparator)
+			someMap[key] = int(value)
+		return headerValue
+
+	def loadStoredMaps(self):
+		self.loadSingleLineFileIntoMap("./wordPosition", Preprocessor.wordCorpusToColIndexMap, '\t', '-')
+		Preprocessor.TotalDocs = self.loadSingleLineFileIntoMap("./wordNumDocs", Preprocessor.wordToNumDocsMap, '\t', '-')
+
+
+	def construct_feature_vetor_matrix(self, doNotLoadStoredMaps = 'Y', isFeatureTfIdf = 0):
+		if not (doNotLoadStoredMaps == 'Y' or doNotLoadStoredMaps == 'y'):
+			self.loadStoredMaps()
+		if isFeatureTfIdf:
+			fileName="./FinalFeatureVectorTFIDF"
+		else:
+			fileName = "./FinalFeatureVector"
+		self.fileFinalFeatureVector = open(fileName, "w+")
 		corpus_length = len(Preprocessor.wordCorpusToColIndexMap)
 		with open("./FeatureVector") as fileobject:
 			for line in fileobject:
 				vector = [0] * corpus_length
 				words = line.split('\t')
-				for word in words:
-					wordRoots = word.split('-')
-					if wordRoots[0] in Preprocessor.wordCorpusToColIndexMap and Preprocessor.wordCorpusToColIndexMap[wordRoots[0]] < corpus_length:
-						if isFeatureTfIdf:
-							tf = wordRoots[1]
-							idf = 0
-							if Preprocessor.TotalDocs > 0 and Preprocessor.wordToNumDocsMap[wordRoots[0]] > 0:
-								idf = math.log(Preprocessor.TotalDocs/Preprocessor.wordToNumDocsMap[wordRoots[0]])
-							vector[Preprocessor.wordCorpusToColIndexMap[wordRoots[0]]] = tf * idf
-						else:
-							vector[Preprocessor.wordCorpusToColIndexMap[wordRoots[0]]] = wordRoots[1]
-					else:
-						print wordRoots
+				if(len(words) > 1):
+					for wordFreqPair in words:
+						(word, wordFreq) = wordFreqPair.split('-')
+						if word in Preprocessor.wordCorpusToColIndexMap and Preprocessor.wordCorpusToColIndexMap[word] < corpus_length:
+							if isFeatureTfIdf:
+								tf = wordFreq
+								idf = 0
+								if Preprocessor.TotalDocs > 0 and Preprocessor.wordToNumDocsMap[word] > 0:
+									idf = math.log(Preprocessor.TotalDocs/Preprocessor.wordToNumDocsMap[word])
+								vector[Preprocessor.wordCorpusToColIndexMap[word]] = tf * idf
+							else:
+								vector[Preprocessor.wordCorpusToColIndexMap[word]] = wordFreq
+						# else:
+						# 	print (word, wordFreq)
 				outputString = ''
 				for column in vector:
 					outputString += str(column)
@@ -181,12 +218,15 @@ class Preprocessor:
 
 
 newPrep = Preprocessor()
+start_time = time.time()
 if(sys.argv[1] == 'Y' or sys.argv[1] == 'y'):
 	newPrep.init_file_parsing()
 generateTfIdf = (sys.argv[2] == 'Y') or (sys.argv[2] == 'y')
 newPrep.construct_feature_vetor_matrix(sys.argv[1],generateTfIdf)
-# newPrep.construct_feature_vetor_matrix()
-	# print len(tokenizedWords)
+print("--- %s seconds ---" % (time.time() - start_time))
 
-	# myTokenizer(abcd, stopWords, tokenizedWords)
-	# print tokenizedWords
+# start_time = time.time()
+# newPrep.init_file_parsing()
+# generateTfIdf = 0
+# newPrep.construct_feature_vetor_matrix('Y',generateTfIdf)
+# print("--- %s seconds ---" % (time.time() - start_time))
